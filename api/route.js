9a7d25bf7fc09c1f66f50c097d575b78411ad514
rjangid <rjangid@suturehealth.com>
2018-01-06 18:43:37 +0530

    added code

---
+++ api/route.js
@@ -0,0 +1,10 @@
+var coinController = require('./modules/coins/controllers/coin-controller');
+
+function setup(app){
+    app.get('/api/coins', coinController.getCoinsDetails);
+    app.get('/api/coins/report', coinController.postReport);
+};
+
+
+
+module.exports = {setup : setup};
